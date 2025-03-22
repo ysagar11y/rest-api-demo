d26bc8406bf5be5c4984d525427628d64f198838
ysagar11y <ysagar11y@gmail.com>
2025-03-22 20:49:14 +0530

    First commit

--- src/main/java/org/springboot/restapi/service/UserServiceImple.java
+++ src/main/java/org/springboot/restapi/service/UserServiceImple.java
@@ -89,6 +89,7 @@ public class UserServiceImple implements UserService {
         catch (Exception e) {
             log.error("Failed to update user: " + e.getMessage());
             throw new UserServiceIssue();
+
         }
     }
 
